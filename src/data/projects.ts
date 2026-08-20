8397b7b2747c6167a70ac9d7d44ec4ac1cc2b1bd
1p0tato1 <hfayala53@gmail.com>
2026-08-20 16:28:20 +0100

    fix client train

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -23,44 +23,63 @@ export interface TrustedClient {
 }
 
 export const trustedClients: TrustedClient[] = [
-  { id: 'paul', file: 'paul.png', url: 'https://www.paul.fr/' },
-  { id: 'attijari-bank', file: 'attijari.png', url: 'https://www.attijaribank.com.tn/' },
-  { id: 'ministere-equipement', file: 'ministere-equipement-full.png', url: 'https://www.mehat.gov.tn/' },
-  { id: 'aefe', file: 'aefe.png', url: 'https://www.aefe.gouv.fr/' },
-  { id: 'office-tourisme', file: 'office-tourisme.png', url: 'https://www.discovertunisia.com/' },
-  { id: 'poste-tunisienne', file: 'poste-tunisienne.png', url: 'https://www.poste.tn/' },
-  { id: 'bt', file: 'bt.png', url: 'https://www.bt.com.tn/' },
-  { id: 'zitouna-takaful', file: 'zitouna-takaful.png', url: 'https://zitounatakaful.com/fr' },
-  { id: 'foire-sousse', file: 'foire-sousse.png', url: '' },
-  { id: 'hopital-sahloul', file: 'hopital-sahloul.webp', url: 'http://hopitalsahloul.tn/' },
+  // Santé
+  { id: 'hopital-sahloul', file: 'hopital-sahloul.png', url: 'http://hopitalsahloul.tn/' },
   { id: 'polyclinique-corniche', file: 'polyclinique-corniche.png', url: 'http://www.clinique-lacorniche.com/' },
-  { id: 'centre-carthage-medical', file: 'centre-international-carthage-medical.webp', url: 'https://carthagemedical.com.tn/', },
-  { id: 'essalem-clinic', file: 'logo-essalem-clinic.png', url: 'https://www.clinique-essalem.com/' },
-  { id: 'leoni', file: 'leoni.png', url: 'https://www.leoni-tunisia.com/' },
-  { id: 'movenpick', file: 'movenpick.svg', url: 'https://movenpick.accor.com/en/africa/tunisia.html' },
+  { id: 'centre-carthage-medical', file: 'carthage-medical.png', url: 'https://carthagemedical.com.tn/' },
+  { id: 'essalem-clinic', file: 'clinique-essalem.png', url: 'https://www.clinique-essalem.com/' },
+  { id: 'chu-farhat-hached', file: 'chu-farhat-hached.png', url: '' },
+  { id: 'zayatine', file: 'clinique-oliviers.png', url: 'https://www.cliniquelesoliviers.net/' },
   { id: 'unimed', file: 'unimed.png', url: 'https://www.unimed.com.tn/' },
+
+  // Hôtellerie & Tourisme
+  { id: 'movenpick', file: 'movenpick.png', url: 'https://movenpick.accor.com/en/africa/tunisia.html' },
   { id: 'rosa-beach', file: 'rosa-beach.png', url: 'https://rosabeach.tn/fr-fr/' },
   { id: 'tej-marhaba', file: 'tej-marhaba.png', url: 'https://tejmarhaba.tn/' },
-  { id: 'riviera-hotel', file: 'riviera_hotel.png', url: 'https://riviera.tn/' },
-  { id: 'magic-hotels', file: 'Magic_Hotels_&_Resorts.jpg', url: 'https://www.magichotelsandresorts.com/' },
+  { id: 'riviera-hotel', file: 'riviera-hotel.png', url: 'https://riviera.tn/' },
+  { id: 'magic-hotels', file: 'magic-hotels.png', url: 'https://www.magichotelsandresorts.com/' },
+  { id: 'abou-sofiane', file: 'abou-sofiane-hotel.png', url: '' },
+  { id: 'royal-thalassa-monastir', file: 'royal-thalassa-monastir.png', url: '' },
+  { id: 'thalassa-mahdia', file: 'thalassa-mahdia.png', url: '' },
+  { id: 'thalassa-sousse', file: 'thalassa-sousse.png', url: '' },
+  { id: 'office-tourisme', file: 'office-tourisme.png', url: 'https://www.discovertunisia.com/' },
+  { id: 'foire-sousse', file: 'foire-sousse.png', url: '' },
+
+  // Banques, Finance & Assurance
+  { id: 'attijari-bank', file: 'attijari.png', url: 'https://www.attijaribank.com.tn/' },
+  { id: 'bt', file: 'bt.png', url: 'https://www.bt.com.tn/' },
+  { id: 'zitouna-takaful', file: 'zitouna-takaful.png', url: 'https://zitounatakaful.com/fr' },
+  { id: 'bh-immo', file: 'bh-immo.png', url: 'http://www.bh-immo.tn/' },
+
+  // Institutions publiques & Éducation
+  { id: 'ministere-equipement', file: 'ministere-equipement-full.png', url: 'https://www.mehat.gov.tn/' },
+  { id: 'aefe', file: 'aefe.png', url: 'https://www.aefe.gouv.fr/' },
+  { id: 'poste-tunisienne', file: 'poste-tunisienne.png', url: 'https://www.poste.tn/' },
+  { id: 'eps-tahar-sfar', file: 'eps-tahar-sfar.png', url: '' },
+  { id: 'fac-med-sousse', file: 'fac-med-sousse.png', url: '' },
+
+  // Industrie
+  { id: 'leoni', file: 'leoni.png', url: 'https://www.leoni-tunisia.com/' },
   { id: 'kaschke-components', file: 'kaschke_components_tunisie.jpeg', url: 'https://bourns.com/products/magnetic-products/kaschke-components/kaschke-capabilities' },
   { id: 'iffco', file: 'iffco.png', url: 'https://iffco.com/' },
-  { id: 'ifrisol', file: 'ifrisol-logo.png', url: 'https://www.ifrisol.solar/' },
-  { id: 'givi', file: 'givi-social.png', url: 'https://givi.it/' },
-  { id: 'jomaa', file: 'jomaa.png', url: 'https://www.jomaa.tn/fr/' },
-  { id: 'arco', file: 'arco.jpg', url: 'https://www.arco-sa.com/' },
-  { id: 'sts', file: 'STS.png', url: 'https://www.ststn.com/' },
+  { id: 'ifrisol', file: 'ifrisol.png', url: 'https://www.ifrisol.solar/' },
+  { id: 'givi', file: 'givi.png', url: 'https://givi.it/' },
+  { id: 'arco', file: 'arco.png', url: 'https://www.arco-sa.com/' },
+  { id: 'sts', file: 'sts.png', url: 'https://www.ststn.com/' },
   { id: 'stp', file: 'stp.png', url: '' },
-  { id: 'stts', file: 'stts.jpeg', url: 'https://www.stts.com.tn/' },
-  { id: 'azurex', file: 'azurex_sarl.jpeg', url: 'https://www.azurex.com.tn/' },
+  { id: 'stts', file: 'stts.png', url: 'https://www.stts.com.tn/' },
+  { id: 'azurex', file: 'azurex.png', url: 'https://www.azurex.com.tn/' },
   { id: 'cogeb', file: 'cogeb.png', url: 'https://cogebgroupe.com/' },
   { id: 'crc', file: 'crc.png', url: 'https://crc-groupe.com/en/' },
   { id: 'nesspri', file: 'nesspri.png', url: 'https://nesspri.com/' },
-  { id: 'spiz', file: 'spiz.jpeg', url: 'https://www.spiz.tn/' },
-  { id: 'bh-immo', file: 'bh-immo.png', url: 'http://www.bh-immo.tn/' },
-  { id: 'zayatine', file: 'logo-zayatine-fr.jpg', url: 'https://www.cliniquelesoliviers.net/' },
-]
+  { id: 'spiz', file: 'spiz.png', url: 'https://www.spiz.tn/' },
 
+  // Autres
+  { id: 'paul', file: 'paul.png', url: 'https://www.paul.fr/' },
+  { id: 'jomaa', file: 'jomaa.png', url: 'https://www.jomaa.tn/fr/' },
+  { id: 'amine', file: 'amine.png', url: '' },
+  { id: 'al-kanaouet', file: 'el-kanaouet.png', url: '' },
+]
 export interface ProjectStat {
   value: number
   suffix: string
